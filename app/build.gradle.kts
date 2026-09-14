27b57d26fa1f010d5bd00960fc7b08d1427f3be2
calonbikers85-commits <calonbikers85@gmail.com>
2026-09-15 01:36:16 +0700

    Add Play Services Auth and OkHttp dependencies
    Added Google Play Services Auth and OkHttp dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,8 +76,8 @@ googleServices { missingGoogleServicesStrategy = MissingGoogleServicesStrategy.W
 // Some unused dependencies are commented out below instead of being removed.
 // This makes it easy to add them back in the future if needed.
 dependencies {
-  implementation(platform(libs.androidx.compose.bom))
-  implementation(platform(libs.firebase.bom))
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
   // implementation(libs.accompanist.permissions)
   implementation(libs.androidx.activity.compose)
   // implementation(libs.androidx.camera.camera2)
